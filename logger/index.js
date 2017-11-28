e4edff85b202c26cde8bc0a63dece21775b9ecf9
Tomasz Libich <t.libich@gmail.com>
2017-11-28 16:57:53 +0000

    Adding missing param

--- logger/index.js
+++ logger/index.js
@@ -13,25 +13,25 @@ const chalk = require('chalk'),
 module.exports = new class Alb3rtLogger {
     error(fileId, message) {
         console.log(chalk.black.bgRedBright(` ${fileId} `), chalk.redBright(`${helpers.currentTimestamp} ${message}`));
-        this.store('error', message);
+        this.store('error', fileId, message);
     }
 
     warn(fileId, message, dbEntry) {
         console.log(warning(`${helpers.currentTimestamp} [${fileId}]`), chalk.yellow(message));
-        this.store('warn', message);
+        this.store('warn', fileId, message);
     }
 
     log(fileId, message, dbEntry) {
         console.log(chalk.gray(`${helpers.currentTimestamp} [${fileId}] ${message}`));
-        (dbEntry ? this.store : noop)('log', message);
+        (dbEntry ? this.store : noop)('log', fileId, message);
     }
 
     info(fileId, message, dbEntry) {
         console.log(chalk.cyan(`${helpers.currentTimestamp} [${fileId}] ${message}`));
-        (dbEntry ? this.store : noop)('info', message);
+        (dbEntry ? this.store : noop)('info', fileId, message);
     }
 
-    store(type, message) {
+    store(type, fileId, message) {
         if (dbGatewayUrl) {
             const body = {
                 _id: `entry-${appId}-${helpers.currentTimestamp}`,
